000a196f57287d28bd2d28c1c3ddb06eb768c86d
Érick Mazzocco <erickmazzocco@hotmail.com>
2021-10-14 11:54:29 -0300

    TextFileToDataTable

---
+++ ElogroupProjetos/Elogroup.Utils/Activities/TextFileToDataTable.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elogroup.Utils
+{
+    [DisplayName("TextFile To DataTable")]
+    [Description("Convert a .txt/.csv file in a DataTable")]
+    public class TextFileToDataTable : CodeActivity
+    {
+        #region Arguments
+        [Category("Input")]
+        [RequiredArgument]
+        [Description("The full path of the .csv/.txt file")]
+        public InArgument<string> File { get; set; }
+
+        [Category("Options")]
+        [Description(@"The quantity of lines that will be skiped")]
+        [DefaultValue(0)]
+        public InArgument<int> HowManyLinesNeedToSkip { get; set; } = 0;
+
+        [Category("Options")]
+        [DefaultValue(false)]
+        [Description(@"There is header inside the text file")]
+        public bool HasHeaders { get; set; } = false;
+
+        [Category("Options")]
+        [Description("Default character: ;")]
+        public InArgument<string> BreakColumns { get; set; } = ";";
+
+        [Category("Options")]
+        [Description("Default character: \\n")]
+        public InArgument<string> BreakLines { get; set; } = "\n";
+
+        [Category("Options")]
+        [DefaultValue(false)]
+        [Description(@"Throw an exception if the quantity of line's columns is diferent from the quantity of datatable's columns")]
+        public bool ThrowExceptionIfThereIsProblemWithLine { get; set; } = false;
+
+        [Category("Output")]
+        public OutArgument<DataTable> OutputDataTable { get; set; }
+
+        [Category("Output")]
+        public OutArgument<List<string>> OutputLinesWithProblem { get; set; }
+        #endregion
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            DataTable result;
+            var LinesWithProblem = new List<string>();
+            try
+            {
+                var textFileToDataTable = new Code.TextFileToDataTable();
+
+                result = textFileToDataTable.Execute(
+                    File.Get(context),
+                    out LinesWithProblem,
+                    HowManyLinesNeedToSkip.Get(context),
+                    HasHeaders,
+                    BreakColumns.Get(context),
+                    BreakLines.Get(context),
+                    ThrowExceptionIfThereIsProblemWithLine                    
+                );
+            }
+            catch (Exception ex)
+            {
+                OutputDataTable.Set(context, null);                
+                throw ex;
+            }
+            finally
+            {
+                OutputLinesWithProblem.Set(context, LinesWithProblem);
+            }
+
+            OutputDataTable.Set(context, result);
+        }
+    }    
+}
---
+++ ElogroupProjetos/Elogroup.Utils/Code/TextFileToDataTable.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elogroup.Utils.Code
+{
+    public class TextFileToDataTable
+    {
+        private string _file;
+        private int _howManyLinesNeedToSkip;
+        private bool _hasHeader;
+        private string _breakColumns;
+        private string _breakLines;
+        private bool _ThrowExceptionIfThereIsProblemWithLine;        
+
+        public DataTable Execute(
+            string file,
+            out List<string> LinesWithProblem,
+            int howManyLinesNeedToSkip = 0,
+            bool hasHeader = true,
+            string breakColumns = ";",
+            string breakLines = "\n",
+            bool ThrowExceptionIfThereIsProblemWithLine = false            
+            )
+        {
+            _file = file;
+            _howManyLinesNeedToSkip = howManyLinesNeedToSkip;
+            _hasHeader = hasHeader;
+            _breakColumns = breakColumns;
+            _breakLines = breakLines;
+            _ThrowExceptionIfThereIsProblemWithLine = ThrowExceptionIfThereIsProblemWithLine;
+
+            return GetDataTableByText(out LinesWithProblem);
+        }
+
+        public DataTable GetDataTableByText(out List<string> LinesWithProblem)
+        {
+            LinesWithProblem = new List<string>();
+
+            var text = File.ReadAllText(_file);
+            if (string.IsNullOrEmpty(text.Trim()))
+                return new DataTable();
+
+            var lines = text.Split(_breakLines.ToCharArray()).ToList();
+
+            if (_howManyLinesNeedToSkip != 0)
+                lines.RemoveAt(_howManyLinesNeedToSkip - 1);
+
+            var dt = new DataTable();
+
+            var firstLineColumns = lines.ElementAt(0).Split(_breakColumns.ToCharArray()).ToArray();
+            if (_hasHeader)
+            {
+                foreach (var column in firstLineColumns)
+                {
+                    int count = 1;
+                    var nameColumn = column;
+                    while (dt.Columns.Contains(nameColumn))
+                    {
+                        count += 1;
+                        nameColumn = column + " " + count;
+                    }
+
+                    dt.Columns.Add(nameColumn);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < firstLineColumns.Length; i++)
+                {
+                    dt.Columns.Add("Column " + (i + 1).ToString());
+                }
+            }
+
+            var linesLength = lines.Skip(_hasHeader ? 1 : 0).Count();
+            for (int i = 0; i < linesLength; i++)
+            {
+                var line = lines.ElementAt(i);
+
+                var columns = line.Split(_breakColumns.ToCharArray());
+
+                if (columns.Length != dt.Columns.Count)
+                {
+                    if (_ThrowExceptionIfThereIsProblemWithLine)
+                        throw new Exception("Line don't have the same quantity of columns");
+
+                    LinesWithProblem.Add("Line number: " + i + " Line Content: " + line);
+                    continue;
+                }
+
+                dt.Rows.Add(columns);
+            }
+
+            return dt;
+        }
+    }
+}
